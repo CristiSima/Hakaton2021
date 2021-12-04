53fb127193e8a36b33b4e53e320f957a79a3124a
ioanacondrea <ioana.condrea@yahoo.com>
2021-12-04 21:46:17 +0200

    Made drop down list with checkboxes

--- static/sliding_menu.js
+++ static/sliding_menu.js
@@ -1,16 +1,25 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("overpass-api-controls").style.width = "150px";
+    document.getElementById("overpass-api-controls").style.height = "auto";
+    var checkList = document.getElementById('list1');
+    checkList.getElementsByClassName('anchor')[0].onclick = function (evt) {
+        if (checkList.classList.contains('visible'))
+            checkList.classList.remove('visible');
+        else
+            checkList.classList.add('visible');
+    }
 }
-
+openNav();
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("overpass-api-controls").style.width = "45px";
+    document.getElementById("overpass-api-controls").style.height = "45px";
 }
 
 function toggleNav() {
-    navSize = document.getElementById("mySidenav").offsetWidth;
-    if (navSize > 0) {
+    navSize = document.getElementById("overpass-api-controls").offsetWidth;
+    if (navSize > 150) {
         return closeNav();
     }
     return openNav();
